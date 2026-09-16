c2403d2d5558f2b34d312d204b03a3bb8d09b9e5
Shant761 <flashbaracademia@gmail.com>
2026-09-16 20:35:07 +0400

    load commercial section styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import {App} from './App';
 import {ClientsSection} from './ClientsSection';
 import {ServicesDetail,ContactForm} from './CommercialSections';
 import './styles.css';
+import './commercial.css';
 import './mobile-story.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
