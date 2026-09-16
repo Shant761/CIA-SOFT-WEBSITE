67e0303cfe27906ba91688106638b46ad6e18085
Shant761 <flashbaracademia@gmail.com>
2026-09-16 20:30:43 +0400

    fix clients section mounting reliably after solutions

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,28 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import { App } from './App';import {ClientsSection} from './ClientsSection';import './styles.css';import './mobile-story.css';ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);requestAnimationFrame(()=>{const solutions=document.querySelector('.solutions');if(solutions&&!document.getElementById('clients-root')){const host=document.createElement('div');host.id='clients-root';solutions.insertAdjacentElement('afterend',host);ReactDOM.createRoot(host).render(<React.StrictMode><ClientsSection/></React.StrictMode>)}});
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {App} from './App';
+import {ClientsSection} from './ClientsSection';
+import './styles.css';
+import './mobile-story.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode><App/></React.StrictMode>
+);
+
+function mountClients(){
+  if(document.getElementById('clients-root')) return true;
+  const solutions=document.querySelector('.solutions');
+  if(!solutions) return false;
+  const host=document.createElement('div');
+  host.id='clients-root';
+  solutions.insertAdjacentElement('afterend',host);
+  ReactDOM.createRoot(host).render(<React.StrictMode><ClientsSection/></React.StrictMode>);
+  return true;
+}
+
+if(!mountClients()){
+  const observer=new MutationObserver(()=>{
+    if(mountClients()) observer.disconnect();
+  });
+  observer.observe(document.getElementById('root')!,{childList:true,subtree:true});
+}
